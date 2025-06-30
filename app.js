9b94366edfa73c88c80cdd64eb9afc9dcd16d754
MahaGit1233 <maharush5409@gmail.com>
2025-06-30 20:30:18 +0530

    Writing file commit

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 const http = require('http');
+const fs=require('fs');
+
 const server=http.createServer((req,res)=>{
     console.log('Server Created');
 
-    res.setHeader('Content-Type','text/html');
+    const url=req.url;
+    const method=req.method;
+
     if(req.url==='/'){
-        res.statusCode=200;
-        res.end('<h1>Hello World</h1>');
+        res.setHeader('Content-Type','text/html');
+
+        res.end(
+            `
+            <form action='/message' method='POST'>
+                <label>Name:</label>
+                <input type='text' name='username'></input>
+                <button type='submit'>Add</button>
+            </form>
+
+            `
+        )
     }
     else{
-        if(req.url==='/pizza'){
-            res.statusCode=200;
-            res.end('<h1>This is your pizza</h1>');
-        }
-        else if(req.url==='/home'){
-            res.statusCode=200;
-            res.end('<h1>Welcome Home</h1>');
-        }
-        else if(req.url==='/about'){
-            res.statusCode=200;
-            res.end('<h1>Welcome to about us</h1>');
-        }
-        else if(req.url==='/node'){
-            res.statusCode=200;
-            res.end('<h1>Welcome to Node JS Project</h1>');
-        }
-        else{
-            res.statusCode=400;
-            res.end('<h1>Page not found</h1>');
+        if(req.url==='/message'){
+            res.setHeader('Content-Type','text/html');
+
+            let dataChunks=[];
+            req.on('data',(chunks)=>{
+                console.log(chunks);
+                dataChunks.push(chunks);
+            })
+
+            req.on('end',()=>{
+                let combineBuffer=Buffer.concat(dataChunks);
+                console.log(combineBuffer.toString());
+                let value=combineBuffer.toString().split('=')[1];
+                console.log(value);
+
+                fs.writeFile('value.txt',value,(err)=>{
+                    res.statusCode=302;
+                    res.setHeader('Location','/');
+                    res.end();
+                })
+            })
         }
     }
 })
